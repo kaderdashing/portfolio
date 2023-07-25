a77eb3f49c6e0e968b0949723563550fe7bb52dd
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2023-07-25 01:43:00 +0100

    fix: fix about me page with my resume elements.

---
+++ data/experience.ts
@@ -0,0 +1,46 @@
+const experiences = [
+  {
+    title: 'Lead Front-end engineer',
+    company: 'Data Master',
+    location: 'Tlemcen',
+    period: 'May 2023 - Now',
+    tasks: [
+      'Projects initialization (templates), choose the tech stack, setting up github pipelines (CI/CD with github actions). also defining best practices and rules for guarantee the code to be clean.',
+      'Mentor other developers inside the team and participate in daily meetings, also doing code reviews.',
+      'Working on design systems and architectures, more focused on client side architectures, which lead to build and maintain UI library for the company and also npm packages, so the Developer experience will be easy.',
+      'Writing unite tests (vitest, React testing library) and E2E tests (cypress, playwright) for the team. Also being the DevOps enginner.',
+    ],
+  },
+  {
+    title: 'Full-Stack Web Developer & UI/UX Designer',
+    company: 'Lemsa Innovation',
+    location: 'Tlemcen',
+    period: 'Oct 2022 - April 2023',
+    tasks: [
+      'Creating high fidelity, easy to use Mobile/Web apps Designs, with smooth prototyping, amazing animations, and building design system for the company.',
+      'Building beautiful UIs, for mobile and web, maintaining the performance and accessibility issues (TDD), and lately building a React component library for the company. also worked on many web apps projects (E- Commerce, Health, Project Management, Open-AI).',
+    ],
+  },
+  {
+    title: 'Full-Stack Web Developer & UI/UX Designer',
+    company: 'Data Master',
+    location: 'Tlemcen',
+    period: 'Oct 2021 - Oct 2022',
+    tasks: [
+      'Creating high fidelity, easy to use Mobile/Web apps Designs, with smooth prototyping, amazing animations, and building design system for the company.',
+      'Building beautiful UIs, for mobile and web, maintaining the performance and accessibility issues (TDD), and lately building a React component library for the company. And helped in Back-end.',
+      'Being the QA engineer, writing unit test (React-Testing-library , jest), integration tests and E2E tests (Cypress.io). Also being the DevOps engineer.',
+    ],
+  },
+  {
+    title: 'Freelancer',
+    location: 'Tlemcen',
+    period: 'June 2018 - Now',
+    tasks: [
+      'Creating high fidelity, easy to use Mobile/Web apps Designs, with smooth prototyping and amazing animations.',
+      'working on making Mobile and Web applications from scratch, websites for companies and startups, E-Commerce websites for some stores, and documentation websites. Worked with client from all the world.',
+    ],
+  },
+];
+
+export default experiences;
--- layouts/MDX/AuthorLayout.tsx
+++ layouts/MDX/AuthorLayout.tsx
@@ -1,5 +1,6 @@
 import Image from '@/components/Image';
 import { PageSEO } from '@/components/SEO';
+import experiences from '@/data/experience';
 import type { Authors } from 'contentlayer/generated';
 import { ReactNode } from 'react';
 
@@ -17,11 +18,11 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="pt-8">
         <div className="mb-8 flex flex-col-reverse items-center justify-between gap-8 md:flex-row md:items-center">
           <div className="max-w-sm text-center md:w-1/2 md:text-left lg:max-w-full">
-            <h1 className="mb-2 font-roboto text-xl font-medium md:text-2xl lg:text-3xl">
-              Hi, I'm Youssouf Kacemi, I'm a UI/UX Designer and Frontend Developer
+            <h1 className="mb-4 font-roboto text-xl font-medium md:text-2xl lg:text-3xl">
+              Hi, I'm {name}, I'm a {occupation}{' '}
+              <span className="font-semibold text-emerald-600">{company}</span>
             </h1>
             <h1 className="font-roboto text-xl font-medium  text-gray-400 md:text-2xl lg:text-3xl ">
-              {/* {occupation} <span className="font-semibold">{company}</span> */}
               Building beautiful and easy to use UIs for web and mobile applications, UI libraries,
               UI/UX Designs.
             </h1>
@@ -32,9 +33,62 @@ export default function AuthorLayout({ children, content }: Props) {
               height={300}
               width={300}
               src={avatar || ''}
-              className="rounded-full object-scale-down grayscale "
+              className="rounded-full object-scale-down shadow-xl grayscale"
             />
           </div>
+        </div>
+        <div className="prose max-w-none pb-8 text-justify text-sm dark:prose-dark md:text-lg xl:col-span-2">
+          <h2>Who am I ?</h2>
+          <p>
+            I am a Front-end Developer and UI/UX Designer, who has 2 years of commercial experience
+            in building beautiful and easy to use websites and Web Applications and Mobiles Apps,
+            and more than 1 years of building , UI libraries, Web Apps, and E-Commerce websites as a
+            Freelancer. Also an open source lover and contributor.
+          </p>
+        </div>
+        <div className="mb-16 flex flex-col justify-start gap-8">
+          <h2 className="text-[1.5rem] font-bold">Work Experience</h2>
+          <ol className="list-none border-l-2 border-emerald-600 dark:border-emerald-500">
+            {experiences.map((experience, idx) => (
+              <li key={idx.toString()}>
+                <div className="flex-start flex items-center">
+                  <div className="-ml-[9px] -mt-2 mr-3 flex h-4 w-4 items-center justify-center rounded-full bg-emerald-600 dark:bg-emerald-500"></div>
+                  <h4 className="-mt-2 text-xl font-semibold">
+                    {experience.title}
+                    {experience.company && (
+                      <>
+                        at{' '}
+                        <span className="font-semibold text-emerald-500">{experience.company}</span>
+                      </>
+                    )}
+                  </h4>
+                </div>
+                <div className="mb-6 ml-6 pb-6">
+                  <span className="text-lg text-emerald-700 transition duration-150 ease-in-out focus:text-emerald-700 hover:text-emerald-700 active:text-emerald-700 dark:text-emerald-400 dark:focus:text-emerald-500 dark:hover:text-emerald-500 dark:active:text-emerald-700">
+                    {experience.location} | {experience.period}
+                  </span>
+                  <ul>
+                    {experience.tasks.map((task) => (
+                      <li
+                        key={task}
+                        className="ml-6 mb-4 mt-2 list-disc text-base text-neutral-600 first:mt-4 dark:text-neutral-300"
+                      >
+                        {task}
+                      </li>
+                    ))}
+                  </ul>
+                  {/* <button
+                    type="button"
+                    className="inline-block rounded bg-emerald-600 px-4 pb-[5px] pt-[6px] text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:bg-emerald-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 hover:bg-emerald-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] active:bg-emerald-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Read more
+                  </button> */}
+                </div>
+              </li>
+            ))}
+          </ol>
         </div>
         <div
           id="mdx"
